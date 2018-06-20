79e206915c162755f4feeaea09e5000e4b8fcb80
xiongjj <jiaojiao.xiong@dianrong.com>
2018-06-20 15:26:00 +0800

    test

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { Icon } from 'antd';
+import Navbar from '../common/navbar';
+
 import './style.less';
 
 class Home extends Component {
@@ -8,23 +9,7 @@ class Home extends Component {
     return (
       <div className="home-wrapper">
         <section className="hero">
-          <header>
-            <div className="wrapper">
-              <a className="logo-wrap">
-                <img src={require('./assets/logo.png')} className="logo" alt="" titl=""/>
-                <span className="logo-title">Read Lover</span>
-              </a>
-              <nav>
-                <ul>
-                  <li><Link to="/book-list">BookStore</Link></li>
-                  <li><a>About</a></li>
-                  <li><a>Contact</a></li>
-                </ul>
-                <a className="login_btn">Login</a>
-              </nav>
-            </div>
-          </header>
-
+          <Navbar />
           <section className="caption">
             <h2 className="caption">Find Your Dream Book</h2>
             <a>Go Hunting&nbsp;&nbsp;<Icon type="arrow-right" /></a>
